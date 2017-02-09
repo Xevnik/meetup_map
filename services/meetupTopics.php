7c41c7d403d093deb53b1314a1c37f2e48a10d7e
taylor_sturtz <taylorsturtz@gmail.com>
2017-02-09 02:41:59 -0800

    sanitized inputs in php files

--- services/meetupTopics.php
+++ services/meetupTopics.php
@@ -4,5 +4,7 @@ header("Access-Control-Allow-Origin: *");
 
 require_once ('apikeys.php');
 
-$getTopicsData = file_get_contents("https://api.meetup.com/topics?search=".$_GET['keyword']."&page=5&key=".$meetupKey1."&sign=true");
+$keyword = filter_var($_GET['keyword'], FILTER_SANITIZE_STRING);
+
+$getTopicsData = file_get_contents("https://api.meetup.com/topics?search=".$keyword."&page=5&key=".$meetupKey1."&sign=true");
 print_r($getTopicsData);

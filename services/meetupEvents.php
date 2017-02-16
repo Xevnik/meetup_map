1618f84e04e5be7fb0c7bda092322e5f3775e481
taylor_sturtz <taylorsturtz@gmail.com>
2017-02-16 11:01:38 -0800

    more input sanitization

--- services/meetupEvents.php
+++ services/meetupEvents.php
@@ -6,6 +6,7 @@ require_once ('apikeys.php');
 
 $zip = filter_var($_GET['zip'], FILTER_SANITIZE_STRING);
 $keyword = filter_var($_GET['keyword'], FILTER_SANITIZE_STRING);
+$keyword = addslashes($keyword);
 
 $getEventsData = file_get_contents("https://api.meetup.com/2/open_events?key=".$meetupKey1."&zip=".$zip."&topic=".$keyword."&page=20");
 print_r($getEventsData);
